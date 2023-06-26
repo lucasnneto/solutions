c449a02720e5447cebc048eec2d07e660be37368
Lucas Neto <61164638+lucasnneto@users.noreply.github.com>
2023-06-26 18:03:31 -0300

    fix

--- debounce_timer.js
+++ debounce_timer.js
@@ -1,20 +1,27 @@
 // Função para aguardar resposta que são sincronas de maneiras async
 
-function waitUntil(callback, tick = 50, attempts = 10) {
+function waitUntil(callback, tick = 50, attempts = 3) {
   let interval;
-  let step = 0;
-  await new Promise((resolve, reject) => {
-    inteval = setInterval(() => {
+  let step = 1;
+ return new Promise((resolve, reject) => {
+  interval = setInterval(() => {
+    console.log(step)
       if (step === attempts) {
         clearInterval(interval);
         reject("Tentativas máximas alcançadas");
       }
       callback((params)=>{
-        clearInterval(interval);
         resolve(params);
+        clearInterval(interval);
       });
       step += 1;
     }, tick);
   });
-  clearInterval(interval);
 }
+waitUntil((resolve) => {
+  resolve('e');
+}).then((resultado) => {
+  console.log('Resultado alcançado:', resultado);
+}).catch((erro) => {
+  console.error('Erro:', erro);
+});
